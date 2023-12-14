e71e3aacfe69974a03ba50bc9cf86534fb100879
amiroo54 <amiroo.f54@gmail.com>
2023-12-14 13:30:39 +0330

    day_14 part 2 (i tried)

--- day_14/day_14.go
+++ day_14/day_14.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"bufio"
+	"bytes"
+	"encoding/gob"
 	"fmt"
 	"os"
 )
@@ -15,20 +17,76 @@ func printPlatform(platform [][]rune) {
 	}
 }
 
-func titlPlatform(platform [][]rune) {
+func titlPlatformNorth(platform [][]rune) [][]rune {
+	newPlat := platform
 	var isStable bool = true
 	for row := 1; row < len(platform); row++ {
 		for col := 0; col < len(platform[row]); col++ {
 			if platform[row][col] == 'O' && platform[row-1][col] == '.' {
-				platform[row][col] = '.'
-				platform[row-1][col] = 'O'
+				newPlat[row][col] = '.'
+				newPlat[row-1][col] = 'O'
 				isStable = false
 			}
 		}
 	}
 	if !isStable {
-		titlPlatform(platform)
+		return titlPlatformNorth(newPlat)
 	}
+	return newPlat
+}
+
+func titlPlatformWest(platform [][]rune) [][]rune {
+	newPlat := platform
+	var isStable bool = true
+	for row := 0; row < len(platform); row++ {
+		for col := 1; col < len(platform[row]); col++ {
+			if platform[row][col] == 'O' && platform[row][col-1] == '.' {
+				newPlat[row][col] = '.'
+				newPlat[row][col-1] = 'O'
+				isStable = false
+			}
+		}
+	}
+	if !isStable {
+		return titlPlatformWest(newPlat)
+	}
+	return newPlat
+}
+
+func titlPlatformSouth(platform [][]rune) [][]rune {
+	newPlat := platform
+	var isStable bool = true
+	for row := 0; row < len(platform)-1; row++ {
+		for col := 0; col < len(platform[row]); col++ {
+			if platform[row][col] == 'O' && platform[row+1][col] == '.' {
+				newPlat[row][col] = '.'
+				newPlat[row+1][col] = 'O'
+				isStable = false
+			}
+		}
+	}
+	if !isStable {
+		return titlPlatformSouth(newPlat)
+	}
+	return newPlat
+}
+
+func titlPlatformEast(platform [][]rune) [][]rune {
+	newPlat := platform
+	var isStable bool = true
+	for row := 0; row < len(platform); row++ {
+		for col := 0; col < len(platform[row])-1; col++ {
+			if platform[row][col] == 'O' && platform[row][col+1] == '.' {
+				newPlat[row][col] = '.'
+				newPlat[row][col+1] = 'O'
+				isStable = false
+			}
+		}
+	}
+	if !isStable {
+		return titlPlatformEast(newPlat)
+	}
+	return newPlat
 }
 
 func getPlatformLoad(platform [][]rune) int {
@@ -36,12 +94,34 @@ func getPlatformLoad(platform [][]rune) int {
 	for row := 0; row < len(platform); row++ {
 		for col := 0; col < len(platform[row]); col++ {
 			if platform[row][col] == 'O' {
-				println(len(platform) - row)
 				totalNum += len(platform) - row
 			}
 		}
 	}
 	return totalNum
+}
+
+func doACycle(platform [][]rune) [][]rune {
+	platform = titlPlatformNorth(platform)
+	platform = titlPlatformWest(platform)
+	platform = titlPlatformSouth(platform)
+	platform = titlPlatformEast(platform)
+	return platform
+}
+
+func Hash[t any](s t) []byte {
+	var b bytes.Buffer
+	gob.NewEncoder(&b).Encode(s)
+	return b.Bytes()
+}
+
+func CompareHash(a, b []byte) bool {
+	a = append(a, b...)
+	c := 0
+	for _, x := range a {
+		c ^= int(x)
+	}
+	return c == 0
 }
 
 func main() {
@@ -55,18 +135,28 @@ func main() {
 	scanner := bufio.NewScanner(file)
 	platform := make([][]rune, 0)
 
+	hashes := make([][]byte, 0)
+
+	hashes = append(hashes, Hash(platform))
 	for scanner.Scan() {
 		var rowText = scanner.Text()
 		row := make([]rune, 0)
 		for char := 0; char < len(rowText); char++ {
-			fmt.Println(rowText[char])
 			row = append(row, rune(rowText[char]))
 		}
 		platform = append(platform, row)
 	}
-	printPlatform(platform)
-	titlPlatform(platform)
-	fmt.Println()
-	printPlatform(platform)
-	println(getPlatformLoad(platform))
+
+	var foundHash bool = false
+
+	for !foundHash {
+		platform = doACycle(platform)
+		thisHash := Hash(platform)
+		for _, hash := range hashes {
+			if CompareHash(hash, Hash(thisHash)) {
+				fmt.Println("found pattern")
+			}
+		}
+		hashes = append(hashes, thisHash)
+	}
 }
